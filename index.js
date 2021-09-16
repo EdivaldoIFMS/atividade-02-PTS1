d4f78801468e5dc70a3a2aeae42dc2916e84f209
Edivaldo Junior <90478451+EdivaldoIFMS@users.noreply.github.com>
2021-09-16 03:34:45 +0000

    github

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-const http = require("http");
+
 const express = require('express');
 const app = express();
 const porta = 3000;
-
-const servidor = http.createServer(function(req, res){
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>Minha Atividade</title></head>');
-  res.write('<body> Edivaldo Jose dos Santos Junior. <br>Informatica 4B</body>');
-  res.write('</html>');
-  res.end();
-});
 
 app.use(express.static('public'));
 app.set( 'view engine', 'ejs') ; 
@@ -21,16 +12,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('/index.ejs');
 });
 
 app.get('/noticias', (req, res) => {
   res.render('noticias/index.ejs');
 });
 
-app.get('/noticias.01' , (req, res) => {
+app.get('/noticias.01', (req, res) => {
   res.render('noticias.01/index.ejs');
 });
+
 
 app.get('/noticias.02', (req, res) => {
   res.render('noticias.02/index.ejs');
